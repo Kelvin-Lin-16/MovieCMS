86df176c2b4c354368053d3f149b834eca6f27de
Kelvin-Lin-16 <kelvin@linsapp.com>
2016-05-17 12:25:04 -1000

    Add movie model

---
+++ WebApplication1/WebApplication1/Models/MovieModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class MovieModels
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1,1000)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+           public DbSet<MovieModels> Movies { get; set; }
+    }
+}
